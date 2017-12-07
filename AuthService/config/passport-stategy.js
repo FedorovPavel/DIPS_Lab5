b919dbb56ad669fb507d489113542a6912ef919e
Pavel <IU7.2@yandex.ru>
2017-12-07 21:18:50 +0300

    Выбрана стратегия без использования appId и appSecret Переписана базовая стратегия для Ouauth2

--- AuthService/config/passport-stategy.js
+++ AuthService/config/passport-stategy.js
@@ -4,43 +4,59 @@ const   config          	= require('./config'),
         passwordStrategy 	= require('passport-oauth2-client-password').Strategy,
         bearerStrategy  	= require('passport-http-bearer').Strategy,
         UserModel   			= require('./../app/models/users').userModel,
-        ClientModel 			= require('./../app/models/client').clientModel,
+        // ClientModel 			= require('./../app/models/client').clientModel,
         AccessToken  			= require('./../app/models/accesstoken').tokenModel,
         RefreshToken 			= require('./../app/models/refreshtoken').tokenModel;
 
-passport.use(new basicStrategy(
-    function(appId, appSecret, done){
-        ClientModel.findOne({clientId : appId}, function(err, app_cli){
-            if (err)
-                return done(err);
-            if (!app_cli)
-                return done(null, false);
-            if (app_cli.clientSecret != appSecret)
-                return done(null, false);
-            return done(null, app_cli);
-        });
-    }
-));
+/* Вариант для appId и appSecret
+	passport.use(new basicStrategy(
+			function(appId, appSecret, done){
+					ClientModel.findOne({clientId : appId}, function(err, app_cli){
+							if (err)
+									return done(err);
+							if (!app_cli)
+									return done(null, false);
+							if (app_cli.clientSecret != appSecret)
+									return done(null, false);
+							return done(null, app_cli);
+					});
+			}
+	));
 
-passport.use(new cliPwdStr(
-    function(clientId, clientSecret, done){
-        console.log('use Pwd strategy');
-        ClientModel.findOne({clientId: clientId}, function(err, client){
-            if (err) 
-                return done(err);
-            if (!client)
-                return done(null, false);
-            if (client.clientSecret != clientSecret)
-                return done(null, false);
-            return done(null, client);
-        });
-    }
+	passport.use(new cliPwdStr(
+			function(clientId, clientSecret, done){
+					console.log('use Pwd strategy');
+					ClientModel.findOne({clientId: clientId}, function(err, client){
+							if (err) 
+									return done(err);
+							if (!client)
+									return done(null, false);
+							if (client.clientSecret != clientSecret)
+									return done(null, false);
+							return done(null, client);
+					});
+			}
+	));
+*/
+
+//  Вариант OAuth без appId и appSecret
+passport.use(new basicStrategy(
+	function(userName, userPwd, done){
+		UserModel.findOne({login : userName}, function(err, user){
+			if (err)
+				return done(err);
+			if (!user)
+				return done(null, false);
+			if (!user.checkPassword(userPwd))
+				return done(null, false);
+			return done(null, user);
+		});
+	}
 ));
 
 passport.use(new bearerStr(
-    function(accessToken, done){
-        console.log('use Bearer strategy');
-        TokenModel.findOne({token : accessToken, tokenType : 'access'},function(err, token){
+  function(accessToken, done){
+      TokenModel.findOne({token : accessToken, tokenType : 'access'},function(err, token){
             if (err)
                 return done(err);
             if (!token)
