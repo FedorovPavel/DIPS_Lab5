93f93a2b718418c47c4f72ddb283d1f353204339
Pavel <IU7.2@yandex.ru>
2017-12-08 11:05:11 +0300

    Изменены стратегии

--- AuthService/config/passport-stategy.js
+++ AuthService/config/passport-stategy.js
@@ -1,8 +1,8 @@
 const   config          	= require('./config'),
         passport       		= require('passport'),
-        basicStrategy   	= require('passport-http').BasicStrategy,
-        passwordStrategy 	= require('passport-oauth2-client-password').Strategy,
-        bearerStrategy  	= require('passport-http-bearer').Strategy,
+				basicStrategy   	= require('passport-http').BasicStrategy,
+				bearerStrategy 		= require('passport-http-bearer').Strategy,
+        //passwordStrategy 	= require('passport-oauth2-client-password').Strategy,
         UserModel   			= require('./../app/models/users').userModel,
         // ClientModel 			= require('./../app/models/client').clientModel,
         AccessToken  			= require('./../app/models/accesstoken').tokenModel,
@@ -22,10 +22,8 @@ const   config          	= require('./config'),
 					});
 			}
 	));
-
-	passport.use(new cliPwdStr(
+	passport.use(new passwordStrategy(
 			function(clientId, clientSecret, done){
-					console.log('use Pwd strategy');
 					ClientModel.findOne({clientId: clientId}, function(err, client){
 							if (err) 
 									return done(err);
@@ -40,13 +38,19 @@ const   config          	= require('./config'),
 */
 
 //  Вариант OAuth без appId и appSecret
+//  Стратегия авторизации по логину-паролю
 passport.use(new basicStrategy(
 	function(userName, userPwd, done){
+		//  Поиск юзера по логину
+		console.log('Проверка пользователя по login|password');
 		UserModel.findOne({login : userName}, function(err, user){
+			//  Если ошибка вернуть ошибку 
 			if (err)
 				return done(err);
+			//  Нет такого пользователя - вернуть провал верификации 
 			if (!user)
 				return done(null, false);
+			//  Если не совпадает пароль - вернуть провал верификации 
 			if (!user.checkPassword(userPwd))
 				return done(null, false);
 			return done(null, user);
@@ -54,27 +58,43 @@ passport.use(new basicStrategy(
 	}
 ));
 
-passport.use(new bearerStr(
+//  Стратегия для Bearer токена
+passport.use(new bearerStrategy(
   function(accessToken, done){
-      TokenModel.findOne({token : accessToken, tokenType : 'access'},function(err, token){
-            if (err)
-                return done(err);
-            if (!token)
-                return done(null, false);
-            if(Math.round((Date.now() - token.lastUse)/1000) > config.get('security:tokenLife')){
-                TokenModel.remove({token : accessToken}, function(err){
-                    if (err) return done(err);
-                });
-                return done(null, false, {message : 'Token expired'});
-            }
-            UserModel.findById(token.userId, function(err, user){
-                if (err)
-                    return done(err);
-                if (!user)
-                    return done(null, false, {message: 'Unkown user'});
-                let info = {scope : '*'};
-                done(null, user, info);
-            });
-        });
-    }
-))
+		//  Ищем токен
+    AccessToken.findOne({token : accessToken},function(err, token){
+			//  Если ошибка вернуть ошибку
+      if (err)
+				return done(err);
+			//  Если токен не найден вернуть провал верификации
+      if (!token)
+				return done(null, false);
+			//  Расчет времени жизни
+			const timeLife = Math.round((Date.now() - token.created)/1000); // В секундах
+      if(timeLife > config.security.tokenLife){
+				//  Время жизни токена истекло надо его удалить 
+        AccessToken.remove({token : accessToken}, function(err){
+          if (err) return done(err);
+				});
+				//  Сообщить что верификация провалена, токен стух
+        return done(null, false, {message : 'Token expired'});
+			}
+			//  Поиск владельца токена
+      UserModel.findById(token.userId, function(err, user){	
+				//  Если ошибка вернуть ошибку
+        if (err)
+					return done(err);
+				//  Если владелец не найден
+        if (!user)
+					return done(null, false, {message: 'Unkown user'});
+				// let info = {
+				// 	access_token 	: token,
+				// 	token_type		:"bearer",
+				// 	expires_in		:config.security.tokenLife
+				// }
+				var info = {scope : '*'};
+        return done(null, user, info);
+      });
+    });
+  }
+));
