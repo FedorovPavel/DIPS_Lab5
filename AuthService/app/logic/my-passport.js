a123b619a05bad6e332013d3fc7d890212d2ef35
Pavel <IU7.2@yandex.ru>
2017-12-12 01:19:54 +0300

    Функции взаимодействия с my-passport.js

--- AuthService/app/logic/my-passport.js
+++ AuthService/app/logic/my-passport.js
@@ -1,6 +1,7 @@
 const   basic       = require('basic-auth'),
         strategy    = require('./../../config/passport-stategy');
 const basicType = /basic/i;
+const bearerType = /bearer/i;
 
 
 module.exports = {
@@ -12,59 +13,74 @@ module.exports = {
         //  Если тип авторизации Basic
         if (type){
             return checkBasicAuthorization(header_authorization, callback);
-        } else {
+        } else if (bearerType.test(header_authorization)){
             //  Если тип авторизации Bearer token
             return checkBearerAuthorization(header_authorization, callback);
         }
+        return callback('Unknown authorization type', 400, null);
     },
     /**
      * Установка token'ов для пользователя
      */
     setUserTokenByPwd : function(log, pwd, callback){
-        return strategy.createTokenForUser(log, pwd, function(err, scope){
+        return strategy.createTokenForUser(log, pwd, function(err, status, scope){
             if (err)
-                return callback(err, null);
+                return callback(err, status, null);
             if (!scope)
-                return callback(null, null);
-            return callback(null, scope);
+                return callback(null, status, null);
+            return callback(null, null, scope);
         });
     },
     setUserTokenByToken : function(token, callback){
-        return strategy.createTokenForUserByToken(token, function(err, scope){
+        return strategy.createTokenForUserByToken(token, function(err, status, scope){
             if (err)
-                return callback(err, null);
+                return callback(err, status, null);
             if (!scope)
-                return callback(null, null);
-            return callback(null, scope);
+                return callback(null, status, null);
+            return callback(null, null, scope);
         });
     },
+    checkUserByBearer : function(header_text , callback){
+        if (!bearerType.test(header_text))
+            return callback('Is not Bearer token', 400, null);
+        const token = getBearer(header_text);
+        return strategy.checkUserByAccessToken(token, function(err, status, user){
+            if (err)
+                return callback(err, status);
+            if (status || !user)
+                return callback('User not found', status);
+            return callback(null, status, user);
+        });
+    }   
 }
 
+//  Проверка авторизации сервиса по Basic аутентификации
 function checkBasicAuthorization(header_authorization, callback){
+    //  Получаем app.name и app.pass
     const service = basic.parse(header_authorization);
-    return strategy.checkService(service.name, service.pass, function(err, application){
+    //  Проверяем сервис с данными name и pass
+    return strategy.checkService(service.name, service.pass, function(err, status, application){
         if (err)
-            return callback(err, null);
+            return callback(err, status, null);
         if (!application)
-            return callback(null, null);
-        return strategy.setNewAccessTokenToApp(application, function(err, scope){
+            return callback(null, status, null);
+        //  Так как авторизация происходила по логину/паролю выдать токен
+        return strategy.setNewAccessTokenToApp(application, function(err, status, scope){
             if (err)
-                return callback(err, null);
-            if (!application)
-                return callback(null, null);
+                return callback(err, status, null);
             if (!scope)
-                return callback(null, null);
-            return callback(null, scope);
+                return callback(null, status, null);
+            return callback(null, null, scope);
         });
     });
 }
 
 function checkBearerAuthorization(header_authorization, callback){
     const serviceToken = getBearer(header_authorization);
-    return strategy.checkServiceAccessToken(serviceToken, function(err, result){
+    return strategy.checkServiceAccessToken(serviceToken, function(err, status, result){
         if (err)
-            return callback(err, null);
-        return callback(null, result);
+            return callback(err, status, null);
+        return callback(null, status, result);
     });
 }
 
