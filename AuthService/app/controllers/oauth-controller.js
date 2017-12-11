08990ab783f0e66e955ecf135975317483e052aa
Pavel <IU7.2@yandex.ru>
2017-12-12 01:20:17 +0300

    рефакторинг ouath контроллера

--- AuthService/app/controllers/oauth-controller.js
+++ AuthService/app/controllers/oauth-controller.js
@@ -7,44 +7,24 @@ module.exports = (app) => {
 };
 
 router.post('/token', function(req, res, next){
-  //  считываем заголовок из header
-  const header_auth = req.headers['authorization'];
+  const header_auth = req.headers['authorization'];     //  считываем заголовок из headers
   //  если такой заголовк есть и он не пуст
   if (header_auth && typeof(header_auth) !== 'undefined'){
     //  Проверим авторизацию сервиса aggregator
-    return passport.checkServiceAuthorization(header_auth, function(err, scope){
+    return passport.checkServiceAuthorization(header_auth, function(err, status, scope){
       //  Если ошибка вернуть ошибку для сервиса
       if (err)
-        return res.status(500).send(getResponseObject('Service error', err));
+        return res.status(status).send(getResponseObject('Service error', err));
       //  Если данные для aggregator не определены вернуть ошибку
       if (!scope)
-        return res.status(500).send(getResponseObject('Service error', 'Scope is undefined'));   
+        return res.status(status).send(getResponseObject('Service error', 'Scope is undefined'));   
       //  Определим тип запроса token
       let type = req.body.grant_type;
       //  Если запрос по паролю
       if (type === 'password'){
-        //  Получить логин пароль
-        const log = req.body.login;
-        const pwd = req.body.password;
-        //  Установить новые токены по паролю и логину
-        return passport.setUserTokenByPwd(log, pwd, function(err, user_scope){
-          //  Если возникла ошибка сообщить
-          if (err)
-            return res.status(500).send(getResponseObject('Error', err));
-          //  Если нет информации о выданных токенах вернуть ошибку
-          if (!user_scope)
-            return res.status(500).send(getResponseObject('Error', 'Scope is null'));
-          // Формирование ответа
-          const data = { content : user_scope };
-          if (scope !== true)
-            data.SERVICE = scope;
-          //  отправить ответ
-          return res.status(200).send(data);
-        });
+        return passwordAuthorization(req, res, next, scope);
       } else if (type === 'refresh_token'){
-        //  Если запрос по refresh token'у
-        const token = req.body.refresh_token;
-        passport.setUserTokenByToken(token,)
+        return refreshTokenAuthorization(req, res, next, scope);
       } else {
         //  Если запрос ни по паролю, ни по токену
         return res.status(400).send(getResponseObject('Error', 'Parametr "grant_type" is undefined'));
@@ -54,6 +34,32 @@ router.post('/token', function(req, res, next){
   return res.status(401).send(getResponseObject('Service error', 'Header "Authorization" is undefined'));
 });
 
+router.get('/userId', function(req, res, next){
+  //  считываем заголовок из header
+  const header_auth = req.headers['authorization'];
+  //  если такой заголовк есть и он не пуст
+  if (header_auth && typeof(header_auth) !== 'undefined'){
+    //  Проверим авторизацию сервиса aggregator
+    return passport.checkServiceAuthorization(header_auth, function(err, status, scope){
+      if (err)
+        return res.status(status).send(getResponseObject('Service error', err));
+      if (!scope)
+        return res.status(status).send(getResponseObject('Service error', 'Scope is null'));
+      const user_auth = req.headers['user-authorization'];
+      if (user_auth && typeof(user_auth) !== 'undefined'){
+        return passport.checkUserByBearer(user_auth, function(err, status, user){
+          if (err)
+            return res.status(status).send(getResponseObject('Error', err));
+          if (!user)
+            return res.status(status).send(getResponseObject('Error', 'User is null'));
+          return res.status(200).send(user.id);
+        });
+      }
+      return res.status(401).send(getResponseObject('Error', 'Header "user-authorization" is undefined'));
+    });
+  }
+  return res.status(401).send(getResponseObject('Service error', 'Header "Authorization" is undefined'));
+});
 
 function getResponseObject(status, response){
   return {
@@ -61,27 +67,65 @@ function getResponseObject(status, response){
     message : response
   };
 }
-//router.post('/token', oauth2.token);
 
-// router.post('/userId', passport.authenticate(['basic', 'bearer'], {session : false}), function(req, res, next){
-//   res.status(200).send(req.user.userId);
-// });
+function passwordAuthorization(req, res, next, service_scope){
+  //  Получить логин пароль
+  const log = req.body.login;
+  const pwd = req.body.password;
+  //  Если логин/пароль не определены отправить bad request
+  if (!log || !pwd || typeof(log) == 'undefined' || typeof(pwd) == 'undefined')
+    return res.status(400).send(getResponseObject('Error', 'Bad request login or password is undefined'));
+  //  Установить новые токены по паролю и логину
+  return passport.setUserTokenByPwd(log, pwd, function(err, status, user_scope){
+    //  Если возникла ошибка сообщить
+    if (err)
+      return res.status(status).send(getResponseObject('Error', err));
+    //  Если нет информации о выданных токенах вернуть ошибку
+    if (!user_scope)
+      return res.status(status).send(getResponseObject('Error', 'User for this password and login is not found'));
+    // Формирование ответа
+    const data = { content : user_scope };
+    if (service_scope !== true)
+      data.service = service_scope;
+    //  отправить ответ
+    return res.status(200).send(data);
+  });
+}
 
-// router.get('/create', function(req, res, next){
-//   let model = mongoose.model('User');
-//   let user = new model({
-//     login: 'moderator',
-//     password : '4444'
-//   });
-//   user.save(function(err, nw_user){
-//     if (err)
-//       return res.send(err);
-//     return res.send(nw_user);
-//   });
-// let ClientModel = mongoose.model('Client');
-// var client = new ClientModel({ name: "OurService iOS client v1", clientId: "mobileV1", clientSecret:"abc123456" });
-// client.save(function(err, client) {
-//     if(err) return log.error(err);
-//     res.send(null);
-// });
-//});
+function refreshTokenAuthorization(req, res, next, service_scope){
+  //  Если запрос по refresh token'у
+  const token = req.body.refresh_token;
+  if (!token || typeof(token) == 'undefined')
+    return res.status(400).send(getResponseObject('Error', 'Token is undefined'));
+  return passport.setUserTokenByToken(token, function(err, status, user_scope){
+    if (err)
+      return res.status(status).send(getResponseObject('Error', err));
+    if (!user_scope)
+      return res.status(status).send(getResponseObject('Error', 'Scope is null'));
+    const data = {content : user_scope};
+    if (service_scope !== true)
+      data.service = service_scope;
+    return res.status(200).send(data);
+  });
+}
+
+/*
+  router.get('/create', function(req, res, next){
+    let model = mongoose.model('User');
+    let user = new model({
+      login: 'moderator',
+      password : '4444'
+    });
+    user.save(function(err, nw_user){
+      if (err)
+        return res.send(err);
+      return res.send(nw_user);
+    });
+  let ClientModel = mongoose.model('Client');
+  var client = new ClientModel({ name: "OurService iOS client v1", clientId: "mobileV1", clientSecret:"abc123456" });
+    client.save(function(err, client) {
+        if(err) return log.error(err);
+        res.send(null);
+    });
+  });
+*/
