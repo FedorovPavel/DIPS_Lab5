4c31d7a2c485c8125146c5d07f8b940b0d58cc75
Pavel <IU7.2@yandex.ru>
2017-12-16 22:36:37 +0300

    Подписка всех обработчиков на проверку app

--- BillingService/app/controllers/api.js
+++ BillingService/app/controllers/api.js
@@ -1,49 +1,78 @@
 const express   = require('express'),
-      router    = express.Router();
-      mongoose  = require('mongoose');
-      billings  = mongoose.model('Billing');
+      router    = express.Router(),
+      mongoose  = require('mongoose'),
+      billings  = mongoose.model('Billing'),
+      passport  = require('./../passport/my-passport');
 
 module.exports = function(app) {
   app.use('/billings', router);
 };
 
 router.post('/', function(req, res, next) {
-  const param = {
-    PaySystem : req.body.paySystem,
-    Account   : req.body.account,
-    Cost      : req.body.cost
-  };
-  billings.createBillingRecord(param, function(err, billing){
-    if (err)
-      res.status(400).send({status:'Error', message : err});
-    else {
-      res.status(201).send(billing);
-    }
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const param = {
+      PaySystem : req.body.paySystem,
+      Account   : req.body.account,
+      Cost      : req.body.cost
+    };
+    return billings.createBillingRecord(param, function(err, billing){
+      if (err)
+        return res.status(400).send({status:'Error', message : err, service : scope});
+      const data = {
+        content : billing,
+        service : scope
+      };
+      return res.status(201).send(data);
+    });
   });
 });
 
-router.get('/:id', function(req, res, next){
-  const id = req.params.id;
-  billings.getBillingRecord(id, function(err, billing){
-    if (err)
-      res.status(400).send({status : 'Error' , message : err});
-    else {
-      if (billing){
-        res.status(200).send(billing);
-      } else {
-        res.status(404).send({status : 'Error', message : 'Billing not found'});
+router.get('/scope', function(req, res, next){
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const ids = String(req.query.ids).split(',');
+    return billings.getBillingsRecordByIDs(ids, function(err, billings){
+      if (err)
+        return res.status(400).send({status : 'Error', message : err, service : scope});
+      if (billings){
+        const data = {
+          content : billings,
+          service : scope
+        };
+        return res.status(200).send(data);
       }
-    }
+      return res.status(404).send({status : 'Error', message : "billings not found", service : scope});
+    });
   });
+}); 
+
+router.get('/:id', function(req, res, next){
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const id = req.params.id;
+    return billings.getBillingRecord(id, function(err, billing){
+      if (err)
+        return res.status(400).send({status : 'Error' , message : err, service : scope});
+      else {
+        if (billing){
+          const data = {
+            content : billing,
+            service : scope
+          };
+          return res.status(200).send(data);
+        } 
+        return res.status(404).send({status : 'Error', message : 'Billing not found', service : scope});
+      }
+    });
+  });  
 });
 
 router.delete('/:id', function(req, res, next){
-  const id = req.params.id;
-  billings.revertBilling(id, function(err, result){
-    if (err)
-      res.status(500).send({status: 'Critical error', message : err});
-    else {
-      res.send({status: 'Ok', message : 'Billing was removed'});
-    }
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const id = req.params.id;
+    return billings.revertBilling(id, function(err, result){
+      if (err)
+        return res.status(500).send({status: 'Critical error', message : err, service : scope});
+      return res.send({status: 'Ok', message : 'Billing was removed', service : scope});
+    });
   });
 });
+
