59f9a95b0ed56ee275976d5d8ff2a8581bed3351
Pavel <IU7.2@yandex.ru>
2017-12-16 21:48:10 +0300

    Подписка всех обработчиков на проверку авторизации сервиса

--- OrderingService/app/controllers/api_service.js
+++ OrderingService/app/controllers/api_service.js
@@ -1,31 +1,35 @@
 const express   = require('express'),
       router    = express.Router(),
       mongoose  = require('mongoose'),
-      orders    = mongoose.model('Order');
+      orders    = mongoose.model('Order'),
+      passport  = require('./../passport/my-passport');
 
 module.exports = function(app) {
   app.use('/orders', router);
 };
 
 router.head('/live',function(req, res, next){
-  res.status(200).send();
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    return res.status(200).send(null);
+  });
 });
 
 router.get('/', function(req, res, next){ 
-  let page  = req.query.page;
-  let count = req.query.count;
-  const id = "59f634f54929021fa8251644";
-  orders.getOrders(id, page, count, function(err, rOrders){
-    if (err) {
-      if (err.kind == 'ObjectId')
-        res.status(400).send({status : 'Error', message : 'Bad request : Invalid ID'});
-      else
-        res.status(400).send({status : 'Error', message : err});
-    } else {
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    let page  = req.query.page;
+    let count = req.query.count;
+    const id  = getUserId(req);
+    return orders.getOrders(id, page, count, function(err, rOrders){
+      if (err) {
+        if (err.kind == 'ObjectId')
+          return res.status(400).send({status : 'Error', message : 'Invalid ID', service : scope});
+        else
+          return res.status(400).send({status : 'Error', message : err, service : scope});
+      }
       if (rOrders) {
-        orders.getCount(function(err, countRecord){
+        return orders.getCount(id, function(err, countRecord){
           if (err)
-            return res.status(500).send({status : 'Error', message : err});
+            return res.status(500).send({status : 'Error', message : err, service : scope});
           let data = {
             content : rOrders,
             info : {
@@ -33,111 +37,135 @@ router.get('/', function(req, res, next){
               pages   : Math.ceil(countRecord / count) - 1,
               current : page,
               limit   : count
-            }
-          }
-          res.status(200).send(data);
+            },
+            service : scope
+          };
+          return res.status(200).send(data);
         });
-      } else {
-        res.status(404).send({status : 'Error', message : 'Not found orders'});
       }
-    }
+      return res.status(404).send({status : 'Error', message : 'Not found orders', service : scope});
+    });
   });
 });
 
 router.get('/:id', function(req, res, next) {
-  const uid = "59f634f54929021fa8251644";
-  const id = req.params.id;
-  orders.getOrder(id, function(err, order){
-    if (err) {
-      if (err.kind == 'ObjectId')
-        res.status(400).send({status : 'Error', message : 'Bad request : Invalid ID'});
-      else 
-        res.status(400).send({status : 'Error', message : err});
-    } else {
-      if (order){
-        res.status(200).send(order);
-      } else {
-        res.status(404).send({status:'Error', message : "Order isn't found"});
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const uid = getUserId(req);
+    const id = req.params.id;
+    return orders.getOrder(uid, id, function(err, order){
+      if (err) {
+        if (err.kind == 'ObjectId')
+          return res.status(400).send({status : 'Error', message : 'Bad request : Invalid ID'});
+        else 
+          return res.status(400).send({status : 'Error', message : err});
       }
-    }
+      if (order){
+        data = {
+          content : order,
+          service : scope
+        };
+        return res.status(200).send(data);
+      }
+      return res.status(404).send({status:'Error', message : "Order isn't found", service : scope});
+    });
   });
 });
 
 router.put('/confirm/:id', function(req, res, next){
-  const id = req.params.id;
-  console.log(id);
-  orders.setWaitStatus(id, function(err, result){
-    console.log(err);
-    if (err) {
-      if (err.kind == "ObjectId")
-        res.status(400).send({status : 'Error', message : 'Bad request: bad ID'});
-      else if (err == "Status don't right")
-        res.status(400).send({status : 'Error', message : err});
-      else 
-        res.status(400).send({status : 'Error', message : err});
-    } else {
-      if (result) {
-        res.status(200).send(result);
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const id = req.params.id;
+    const uid = getUserId(req);
+    return orders.setWaitStatus(uid, id, function(err, result){
+      if (err) {
+        if (err.kind == "ObjectId")
+          return res.status(400).send({status : 'Error', message : 'Bad request: bad ID', service : scope});
+        else 
+          return res.status(400).send({status : 'Error', message : err, service : scope});
       } else {
-        res.status(404).send({status : 'Error', message : 'Not found order'});
+        if (result) {
+          const data = {
+            content : result,
+            service : scope
+          };
+          return res.status(200).send(data);
+        }
+        return res.status(404).send({status : 'Error', message : 'Not found order'});
       }
-    }
+    });
   });
 });
 
 router.put('/:id/paid/:bid', function(req, res, next){
-  const id = req.params.id;
-  const billing_id = req.params.bid;
-  orders.setPaidStatus(id, billing_id, function(err, result){
-    if (err){
-      if (err.kind == "ObjectId")
-        res.status(400).send({status : 'Error', message : 'Bad request:Bad ID'});
-      else
-        res.status(400).send({status : 'Error', message : err});
-    } else {
-      if (result) {              
-        res.status(200).send(result);
-      } else {
-        res.status(404).send({status : 'Error', message : 'Order not found'});
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const id = req.params.id;
+    const bid = req.params.bid;
+    const uid = getUserId(req);
+    return orders.setPaidStatus(uid, id, bid, function(err, result){
+      if (err){
+        if (err.kind == "ObjectId")
+          return res.status(400).send({status : 'Error', message : 'Bad request:Bad ID', service : scope});
+        else
+          return res.status(400).send({status : 'Error', message : err, service: scope});
       }
-    }
+      if (result) {            
+        const data = {
+          content : result,
+          service : scope
+        }  
+        return res.status(200).send(data);
+      }
+      return res.status(404).send({status : 'Error', message : 'Order not found', service : scope});
+    });
   });
 });
 
 router.put('/complete/:id', function(req, res, next){
-  const id = req.params.id;
-  orders.setCompleteStatus(id, function(err, result){
-    if (err) {
-      if (err.kind == "ObjectId")
-        res.status(400).send({status : 'Error', message : 'Bad ID'});
-      else
-        res.status(400).send({status : 'Error', message : err});
-    } else {
-      if (result) {
-        res.status(202).send(result);
-      } else {
-        res.status(404).send({status : 'Error', message : 'Not found order'});
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    const id = req.params.id;
+    const uid = getUserId(req);
+    return orders.setCompleteStatus(uid, id, function(err, result){
+      if (err) {
+        if (err.kind == "ObjectId")
+          return res.status(400).send({status : 'Error', message : 'Bad ID', service : scope});
+        else
+          return res.status(400).send({status : 'Error', message : err, service : scope});
       }
-    }
+      if (result) {
+        const data = {
+          content : result,
+          service : scope
+        };
+        return res.status(202).send(data);
+      }
+      return res.status(404).send({status : 'Error', message : 'Not found order', service : scope});
+    });
   });
 });
 
 router.post('/', function(req, res, next){
-  let item = {
-    UserID    : req.body.userID,
-    CarID     : req.body.carID,
-    StartDate : req.body.startDate,
-    EndDate   : req.body.endDate
-  };
-  orders.createOrder(item, function(err, result){
-    if (err)
-      return res.status(400).send({status: 'Error', message : err});
-    else {
+  return passport.checkServiceAuthorization(req, res, function(scope){
+    let item = {
+      UserID    : getUserId(req),
+      CarID     : req.body.carID,
+      StartDate : req.body.startDate,
+      EndDate   : req.body.endDate
+    };
+    return orders.createOrder(item, function(err, result){
+      if (err)
+        return res.status(400).send({status: 'Error', message : err, service : scope});
       if (result) {
-        res.status(201).send(result);
-      } else {
-        res.status(500).send({status : 'Error', message : "Order don't create"});
+        const data = {
+          content : result,
+          service : scope
+        };
+        return res.status(201).send(result);
       }
-    }
+      return res.status(500).send({status : 'Error', message : "Order don't create", service : scope});
+    });
   });
 });
+
+function getUserId(req){
+  const id = req.headers['userid'];
+  return id;
+}
